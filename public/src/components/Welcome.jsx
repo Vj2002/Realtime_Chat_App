20cc25bb40271e3014ef67bccb1ea9c230a400cc
Vaibhav Jain <vaibhav.jain20122002@gmail.com>
2022-10-02 14:57:54 +0530

    Initial commit

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome({ currentUser }) {
+//   const [userName, setUserName] = useState("");
+//   useEffect(() => {
+//     const a = async () => {
+//       setUserName(
+//         await JSON.parse(
+//           localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+//         ).username
+//       );
+//     };
+//     a();
+//   }, []);
+
+  return (
+    <Container>
+      <img src={Robot} alt="Robot" />
+      <h1>
+        Welcome, <span>{currentUser.username}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
