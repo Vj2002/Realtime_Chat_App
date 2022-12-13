9a238cf49811e5a5f2b4c0aa44a26918dd65f1bd
Vaibhav Jain <vaibhav.jain20122002@gmail.com>
2022-12-13 22:33:55 +0530

    chat-it

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
     // origin: "https://chat-it.onrender.com/",
-    credentials: false,
+    credentials: true,
   },
 });
 
