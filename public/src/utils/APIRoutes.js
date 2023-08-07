5b783a951bb4c79521b627897a4ba9029ce04b3d
Vaibhav Jain <80044280+Vj2002@users.noreply.github.com>
2023-08-07 11:54:48 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-it.onrender.com";
 // export const host = "https://chat-it.onrender.com/";
 // export const host = "https://vj-chat-app.herokuapp.com/";
 export const registerRoute = `${host}/api/auth/register`;
