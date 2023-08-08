fb2889d3e15c543bc716ebfda97f0ba6973956b4
Vaibhav Jain <80044280+Vj2002@users.noreply.github.com>
2023-08-08 19:05:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const server = app.listen(process.env.PORT, () =>
 const io = socket(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: "https://realtime-chat-app-pied.vercel.app/",
+    origin: "https://realtime-chat-app-pied.vercel.app",
     // origin: "*",
     // methods: "*",
     // origin: "https://chat-it.onrender.com/",
